163666516a4e65f2af6c275f266a4e3a7710a0c2
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-08-07 13:21:14 +0300

    users-model: add validation, and signal

--- users/admin.py
+++ users/admin.py
@@ -2,10 +2,29 @@ from django.contrib import admin
 from .models import App_users
 # Register your models here.
 
+class UserTypeFilter(admin.SimpleListFilter):
+    title = 'User type'
+    parameter_name = 'user_type'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('PUB', 'Publisher'),
+            ('SUB', 'Viewer'),
+            ('NaN', 'No Active'),
+        ]
+    def queryset(self, request, queryset):
+
+        if self.value()=="PUB":
+            return queryset.filter(user_type='PUB')
+        if self.value()=="SUB":
+            return queryset.filter(user_type='SUB')
+        if self.value()=="NaN":
+            return queryset.filter(user_type='NaN')
+        return queryset
 
 class AppUsersAdminModel(admin.ModelAdmin):
     model = App_users
-    list_filter = ['is_publisher']
+    list_filter = ['is_active', UserTypeFilter]
     search_fields = ['email', 'username']
     list_display = ['username', 'email', 'is_publisher', 'user_type']
 
--- users/models.py
+++ users/models.py
@@ -1,16 +1,31 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
-
-USERS_TYPE = [("publisher", "PUB"), ("viewer", "SUB")]
+USERS_TYPE = [("PUB", "publisher"), ("SUB", "viewer"), ("NaN", "NO-ACTIVE")]
 
 # Create your models here.
 class App_users(AbstractUser):
-    is_publisher = models.BooleanField(default=False)
-    user_type = models.TextField(max_length=20, choices=USERS_TYPE, default="viewer")
+    user_type = models.TextField(max_length=20, choices=USERS_TYPE, default=USERS_TYPE[1])
+
+    @property
+    def is_publisher(self):
+        return self.user_type == "PUB"
+
+    def clean(self):
+        if not self.is_active and self.user_type == "NaN":
+            return ValidationError("inactive user can't be anything else 'NO-ACTIVE' ")
+        return super().clean()
 
     def __str__(self):
         return super().get_full_name()
 
     class Meta:
         pass
+
+@receiver(pre_save, sender=App_users)
+def set_user_type_inactive(sender, instance, **kwargs):
+    if instance.is_active is False and instance.user_type !="NaN":
+        instance.user_type = "NaN"
