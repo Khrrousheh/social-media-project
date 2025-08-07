33286cb27b9391f3fea0867fb95206472777db63
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-08-07 11:00:32 +0300

    users_app: apis exposing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import App_users
+from .serializers import UsersSerializers
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = App_users.objects.all()
+    serializer_class = UsersSerializers
+
+    def all_publishers(self):
+        pass
+
+    def all_subscribers(self):
+        pass
